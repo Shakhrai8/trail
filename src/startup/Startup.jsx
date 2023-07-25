a377b24e60c501ab45601d4a65293bf0ed864aa4
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-25 22:49:20 +0100

    add map component with a current location marker, updated package with google map library, startup component will display google map now

--- src/startup/Startup.jsx
+++ src/startup/Startup.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Map from "../map/Map";
+
+const defaultPosition = {
+  lat: 0,
+  lng: 0,
+};
 
 const Startup = () => {
+  const [position, setPosition] = useState(defaultPosition);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setPosition({ lat: latitude, lng: longitude });
+      },
+      () => setPosition(defaultPosition), // Use default position if location access is denied
+      { timeout: 30000 } // Timeout after 30 seconds
+    );
+  }, []);
+
   return (
     <div className="root">
       <h1>Startup</h1>
+      <Map center={position} zoom={10} />
       <Link to="/locations">Start Trail</Link>
     </div>
   );
