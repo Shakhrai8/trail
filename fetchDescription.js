dd3e4434946be2506d877e16c3a8bd5bbf5e661d
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-25 13:01:41 +0100

    add function to fetch the description based by location name by OpenAi api

---
+++ fetchDescription.js
@@ -0,0 +1,37 @@
+const fetch = require("node-fetch");
+
+const getDescription = async ({ queryKey }) => {
+  const locationName = queryKey[1];
+
+  const data = {
+    model: "gpt-3.5-turbo-16k",
+    messages: [
+      {
+        role: "user",
+        content: `tell me a short history of ${locationName}`,
+      },
+    ],
+  };
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": JSON.stringify(data).length,
+      Authorization: `Bearer ${openAiApiKey}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `OpenAI API request failed with status: ${response.status}`
+    );
+  }
+
+  const responseData = await response.json();
+
+  return responseData.choices[0].message.content.trim();
+};
+
+module.exports = getDescription;
