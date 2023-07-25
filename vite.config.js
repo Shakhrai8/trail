ab6edb854a0bd3fad70b129a83570d5cfa3b0995
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-25 14:46:58 +0100

    updated config for google api needs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "./src",
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://maps.googleapis.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
