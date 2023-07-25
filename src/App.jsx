35c5707ceda16b3fb28d4ae0c008528e307ffc51
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-25 14:06:30 +0100

    imported query functionality for the client

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Startup from "./startup/Startup";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <header>
           <Link to="/">Trail</Link>
         </header>
         <Routes>
           <Route path="/" element={<Startup />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
