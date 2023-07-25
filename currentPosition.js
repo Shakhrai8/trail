f658acd437fcb6edd93d05366e223cb0500d03e9
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-25 13:10:33 +0100

    add fetching function for getting current position from Google maps Api

---
+++ currentPosition.js
@@ -0,0 +1,22 @@
+const googleApiKey = "***REMOVED***";
+
+const getCurrentPosition = async () => {
+  const response = await fetch(
+    `https://www.googleapis.com/geolocation/v1/geolocate?key=${googleApiKey}`,
+    {
+      method: "POST",
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `Google Geolocation API request failed with status: ${response.status}`
+    );
+  }
+
+  const data = await response.json();
+
+  return data.location;
+};
+
+module.exports = getCurrentPosition;
