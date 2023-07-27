ae8dcf401e3ca6378b3240880d529143e6eb5053
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-27 16:26:00 +0100

    add static and animated logo components, add greeting screen component

---
+++ src/logo/AnimatedLogo.jsx
@@ -0,0 +1,9 @@
+const AnimatedLogo = () => {
+  return (
+    <div className="logo-container">
+      <iframe src={"/logo.html"} title="Logo" scrolling="no" frameBorder="0" />
+    </div>
+  );
+};
+
+export default AnimatedLogo;
