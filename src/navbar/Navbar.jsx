49df27b775dfa9c96767305d06cf3df41b9483ad
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-29 12:57:57 +0100

    navbar component rework

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -2,39 +2,38 @@ import { useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = ({ googleMapsUrl }) => {
-  const [active, setActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleActive = (e) => {
-    e.preventDefault();
-    setActive(!active);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <div className={styles["sideways-navbar"]}>
-      <nav style={{ "--count": "4", "--active": active ? "1" : "0" }}>
-        <ul onClick={toggleActive}>
-          <li style={{ "--index": "1" }}>
-            <a href="#" target="_blank" rel="noreferrer noopener">
-              <span>More Details</span>
-            </a>
-          </li>
-          <li style={{ "--index": "2" }}>
-            <a href={googleMapsUrl} target="_blank" rel="noreferrer noopener">
-              <span>Google Maps</span>
-            </a>
-          </li>
-          <li style={{ "--index": "3" }}>
-            <a href="#" target="_blank" rel="noreferrer noopener">
-              <span>Placeholder_1</span>
-            </a>
-          </li>
-          <li style={{ "--index": "4" }}>
-            <a href="#" target="_blank" rel="noreferrer noopener">
-              <span>Placeholder_2</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <ul>
+        <li>
+          <button onClick={toggleMenu}>More Details</button>
+        </li>
+        {isOpen && (
+          <>
+            <li>
+              <a href={googleMapsUrl} target="_blank" rel="noreferrer noopener">
+                Google Maps
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank" rel="noreferrer noopener">
+                Placeholder_1
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank" rel="noreferrer noopener">
+                Placeholder_2
+              </a>
+            </li>
+          </>
+        )}
+      </ul>
     </div>
   );
 };
