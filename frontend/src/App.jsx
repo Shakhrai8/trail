a53b1df2cd89bde738c23800355c8a7fce479f20
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-31 16:45:11 +0100

    add functions for marking the places as visited, add functions for saving routes with fetch to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,11 @@ const App = () => {
   // const [locations, setLocations] = useState([]);
   const [isLoadingFirst, setIsLoadingFirst] = useState(false);
   const [error, setError] = useState(null);
+  const [route, setRoute] = useState({
+    start: null,
+    end: null,
+    visited: [],
+  });
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -51,6 +56,78 @@ const App = () => {
     fetchData();
   }, []);
 
+  const markStart = (position) => {
+    setRoute((prevRoute) => ({
+      ...prevRoute,
+      start: position,
+    }));
+  };
+
+  const markVisited = (location, description) => {
+    setRoute((prevRoute) => ({
+      ...prevRoute,
+      visited: [...prevRoute.visited, { ...location, description }],
+      end: location,
+    }));
+  };
+
+  const saveRoute = async () => {
+    const routeName = prompt("Please enter a name for this route:");
+    if (routeName === null || routeName === "") {
+      alert("You must provide a name for the route.");
+      return;
+    }
+
+    const routeDescription = prompt(
+      "Please enter a description for this route:"
+    );
+    if (routeDescription === null || routeDescription === "") {
+      alert("You must provide a description for the route.");
+      return;
+    }
+
+    const routeData = {
+      name: routeName,
+      routeDescription: routeDescription,
+      startingPoint: {
+        lat: route.start.lat,
+        lng: route.start.lng,
+      },
+      endingPoint: {
+        lat: route.end.geometry.location.lat,
+        lng: route.end.geometry.location.lng,
+      },
+      visitedLocations: route.visited.map((location) => ({
+        lat: location.geometry.location.lat,
+        lng: location.geometry.location.lng,
+        placeId: location.place_id,
+        placeName: location.name,
+        description: location.description,
+      })),
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/route/save", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(routeData),
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      alert("Route saved successfully!");
+    } catch (err) {
+      console.error(err);
+      alert("Failed to save route!");
+    }
+  };
+
   return (
     <>
       {showGreeting && <GreetingScreen />}
@@ -62,7 +139,12 @@ const App = () => {
             </Link>
           </header>
           <Routes>
-            <Route path="/" element={<Startup error={error} data={data} />} />
+            <Route
+              path="/"
+              element={
+                <Startup error={error} data={data} markStart={markStart} />
+              }
+            />
             <Route
               path="/locations"
               element={
@@ -70,12 +152,15 @@ const App = () => {
                   isLoading={isLoadingFirst}
                   error={error}
                   data={data}
+                  saveRoute={saveRoute}
                 />
               }
             />
             <Route
               path="locations/:id"
-              element={<Location error={error} data={data} />}
+              element={
+                <Location error={error} data={data} markVisited={markVisited} />
+              }
             />
           </Routes>
         </BrowserRouter>
