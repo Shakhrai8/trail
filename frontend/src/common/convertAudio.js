bf2ba07a68fb55a9dd9c03679d92b202fac2574d
jannisjr <joe_hannis@hotmail.com>
2023-08-01 19:18:10 +0100

    removed unneccesary comments

--- frontend/src/common/convertAudio.js
+++ frontend/src/common/convertAudio.js
@@ -1,26 +1,21 @@
 import { useEffect } from "react";
- const convertAudio = (setIsPlaying, result, audioRef) => {
+const convertAudio = (setIsPlaying, result, audioRef) => {
+  useEffect(() => {
+    if (!result.audio || !result.audio.data) return;
 
- useEffect(() => {
-   // Check if result.audio and result.audio.data exist
-   if (!result.audio || !result.audio.data) return;
+    const audioBlob = new Blob([Uint8Array.from(result.audio.data)], {
+      type: "audio/mpeg",
+    });
+    const audioUrl = URL.createObjectURL(audioBlob);
 
-   const audioBlob = new Blob([Uint8Array.from(result.audio.data)], {
-     type: "audio/mpeg",
-   });
-   const audioUrl = URL.createObjectURL(audioBlob);
+    if (audioRef.current) {
+      audioRef.current.src = audioUrl;
+    }
 
-   if (audioRef.current) {
-     // Set the src attribute of the audio element to the generated URL
-     audioRef.current.src = audioUrl;
-   }
-
-   // Clean up the URL object when the component unmounts
-   return () => {
-     URL.revokeObjectURL(audioUrl);
-   };
- }, [result]);
- 
+    return () => {
+      URL.revokeObjectURL(audioUrl);
+    };
+  }, [result]);
 };
 
 export default convertAudio;
