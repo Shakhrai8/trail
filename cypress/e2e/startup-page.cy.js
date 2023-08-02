407df7c58dfe1138bbaa25f66e33ceb270fb8603
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-02 16:21:42 +0100

    startup page cypress test are fixed

--- cypress/e2e/startup-page.cy.js
+++ cypress/e2e/startup-page.cy.js
@@ -29,26 +29,24 @@ describe("Startup Page", () => {
         })
         .as("ChatGPT")
     );
+    cy.visit("/");
+    cy.wait(5000);
   });
 
-  it("Loads the Startup page", () => {
-    cy.visit("/");
-    cy.contains("Startup").should("be.visible");
+  it("Loads the Startup page with start trail button", () => {
+    cy.contains("Start trail").should("be.visible");
   });
 
   it("Loads the Startup page, clicks link at top, returns to startup", () => {
-    cy.visit("/");
     cy.get("#header-link").click();
-    cy.contains("Startup").should("be.visible");
+    cy.contains("Start trail").should("be.visible");
   });
 
   it("Makes a request to the Google Maps API", () => {
-    cy.visit("/");
     cy.get("@mapsRequest").should("have.been.called");
   });
 
   it("Opens the locations page when the link is clicked", () => {
-    cy.visit("/");
     cy.get("#start-trail").click().wait(2000);
     cy.url().should("include", "/locations");
   });
